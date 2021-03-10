1c5e901076b9eaf3fa6f66eae4bacf9e7b0f3308
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-03-10 16:19:40 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 
-function App() {
+import logo from './assets/images/logo.svg';
+
+export const App = _ => {
+  const AppS = css`
+    text-align: center;
+  `;
+  const AppLogos = css`
+    height: 40vmin;
+    pointer-events: none;
+    @media (prefers-reduced-motion: no-preference) {    
+      animation: App-logo-spin infinite 20s linear;
+    }
+    @keyframes App-logo-spin {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+  `;
+
+  const AppHeaders = css`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  `;
+
+  const AppLink = css`
+    color: #61dafb;
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div css={AppS}>
+      <header css={AppHeaders}>
+        <img src={logo} css={AppLogos} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          css={AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
@@ -21,5 +57,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
