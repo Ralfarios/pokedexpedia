73ced3f2011579e4ce222852b15c4f1c2f7fb762
Muamar Al Farabi <muamaralfarabi@gmail.com>
2021-03-11 11:43:06 +0700

    cleaning 3/11/2021

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -25,13 +25,15 @@ export const NotFound = _ => {
   const handleBackHome = _ => history.push(path.pokemonList);
 
   return (
-    <div id="NotFound" css={css`display: flex; justify-content: center; height:100vh`}>
+    <div id="NotFound" css={css`display: flex; justify-content: center; height:100vh; margin-left: 16px; margin-right: 16px`}>
       <MetaDecorator title="404 Page Not Found" desc="If you see this page, that means the url is wrong." />
 
       <div css={css`align-self: center; text-align: center;`}>
         <img src={illustration} alt="snorlax404" css={illustrationStyle} />
         <h1 style={{ textAlign: 'center', marginBottom: 8, color: '#275d5f' }}>Whoopsy!</h1>
-        <p style={{ textAlign: 'center', marginTop: 0 }}>A wild <b style={{color: '#275d5f'}}>Snorlax</b> has blocked your path!</p>
+        <p style={{ textAlign: 'center', marginTop: 0 }}>
+          The path that wild <b style={{ color: '#275d5f' }}>Snorlax</b> block might be lead to nowhere!
+        </p>
         <Button status="404" as="button" onClick={handleBackHome}>Back Home</Button>
       </div>
     </div>
